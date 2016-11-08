1cb4596deac7b014b4ef4d29c00d306558a1b710
kipernicus <kip.armstrong@gmail.com>
2016-11-08 11:19:03 -0700

    Add more courses to dropdown

--- client/courseRater.js
+++ client/courseRater.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import axios from 'axios'
 import StarRatingComponent from 'react-star-rating-component'
 
 export default class extends React.Component {
@@ -7,9 +8,11 @@ export default class extends React.Component {
 
     this.state = {
       courses: [
-        {id: '12345', name: 'Intro to Intros'},
-        {id: '23456', name: 'Intermediate Intermediaries'},
-        {id: '34567', name: 'Advanced Advancements'}
+        {id: '635de2ee-cfda-459a-9e8e-dff590f76e42', name: 'Advanced Advancements'},
+        {id: '37c82b8d-dbd9-4aa4-869d-47c325efd38a', name: 'An Overview of Overviews'},
+        {id: 'f429e95c-b4c4-4724-a3e4-1b2ffce82278', name: 'Beginnings for Beginners'},
+        {id: 'fa96b764-87a4-4757-90c8-26e214c5cfea', name: 'Intermediate Intermediaries'},
+        {id: 'd5a72023-63b2-2491-8a33-6762ef189d34', name: 'Intro to Intros'}
       ],
       ratings: [ ],
       email: ''
