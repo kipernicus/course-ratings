64fefa69abb75c8af7d2af954427d6de62a6cda0
kipernicus <kip.armstrong@gmail.com>
2016-11-08 11:24:46 -0700

    Move courses into their own module

--- client/courseRater.js
+++ client/courseRater.js
@@ -1,19 +1,14 @@
 import React from 'react'
 import axios from 'axios'
 import StarRatingComponent from 'react-star-rating-component'
+import { courses } from './courses'
 
 export default class extends React.Component {
   constructor() {
     super();
 
     this.state = {
-      courses: [
-        {id: '635de2ee-cfda-459a-9e8e-dff590f76e42', name: 'Advanced Advancements'},
-        {id: '37c82b8d-dbd9-4aa4-869d-47c325efd38a', name: 'An Overview of Overviews'},
-        {id: 'f429e95c-b4c4-4724-a3e4-1b2ffce82278', name: 'Beginnings for Beginners'},
-        {id: 'fa96b764-87a4-4757-90c8-26e214c5cfea', name: 'Intermediate Intermediaries'},
-        {id: 'd5a72023-63b2-2491-8a33-6762ef189d34', name: 'Intro to Intros'}
-      ],
+      courses: courses,
       ratings: [ ],
       email: ''
     };
---
+++ client/courses.js
@@ -0,0 +1,8 @@
+export const courses =
+  [
+    {id: '635de2ee-cfda-459a-9e8e-dff590f76e42', name: 'Advanced Advancements'},
+    {id: '37c82b8d-dbd9-4aa4-869d-47c325efd38a', name: 'An Overview of Overviews'},
+    {id: 'f429e95c-b4c4-4724-a3e4-1b2ffce82278', name: 'Beginnings for Beginners'},
+    {id: 'fa96b764-87a4-4757-90c8-26e214c5cfea', name: 'Intermediate Intermediaries'},
+    {id: 'd5a72023-63b2-2491-8a33-6762ef189d34', name: 'Intro to Intros'}
+  ];
