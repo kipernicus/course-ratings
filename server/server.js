e8cf9c4ad690c6bdd9e306332e6cf388fadf5442
kipernicus <kip.armstrong@gmail.com>
2017-01-11 16:07:09 -0700

    Point to correct folder when running in server mode

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ var express = require('express');
 
 var app = express();
 
-app.use(express.static('client'));
+app.use(express.static('dist'));
 app.listen(3020, function() {
   console.log('Course Rater listening on port 3020!');
 });
